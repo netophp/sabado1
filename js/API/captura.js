d8dacd7f1f90b3432b55aab86a474fae4e16cc74
netophp <netophp@gmail.com>
2013-03-02 12:33:50 -0600

    h
    h

--- js/API/captura.js
+++ js/API/captura.js
@@ -1,32 +1,28 @@
 // Captura imagen
 function capturaImg(){
 
-/*
+
 // capture callback
 var captureSuccess = function(mediaFiles) {
     var ruta;
         ruta = mediaFiles[0].fullPath;
         // do something interesting with the file
+		
+		pgAlert('Captura de imagen','Foto tomada satisfactoriamente');
+		$('#regFoto').attr('rel',ruta).css('background-color','#0F0');
+		$('#login div[data-role=content] #regSubmit').prepend('<img src="'+ruta+'" style="width:100%" />');
 		return ruta;
-};*/
-/*
+};
+
 // capture error callback
 var captureError = function(error) {
     navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
 };
 
-// start image capture*/
-
-
-navigator.device.capture.captureImage(function(mediaFiles) {
-	var ruta="";
-        ruta = mediaFiles[0].fullPath;
-
-}, function(error) {
-    navigator.notification.alert('Código de error: ' + error.code);
-}, {limit:1});
+// start image capture
+navigator.device.capture.captureImage(captureSuccess, captureError, {limit:1});
 
 
 
-return ruta;
+return captureSuccess;
 }
