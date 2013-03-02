dd62876db5a8b24cf815250b91f454f266d6a917
netophp <netophp@gmail.com>
2013-03-02 12:19:01 -0600

    h
    h

--- js/API/captura.js
+++ js/API/captura.js
@@ -1,14 +1,14 @@
 // Captura imagen
 function capturaImg(){
 
-
+/*
 // capture callback
 var captureSuccess = function(mediaFiles) {
     var ruta;
         ruta = mediaFiles[0].fullPath;
         // do something interesting with the file
 		return ruta;
-};
+};*/
 
 // capture error callback
 var captureError = function(error) {
@@ -16,9 +16,14 @@ var captureError = function(error) {
 };
 
 // start image capture
-navigator.device.capture.captureImage(captureSuccess, captureError, {limit:1});
+navigator.device.capture.captureImage(function(mediaFiles) {
+    var ruta;
+        ruta = mediaFiles[0].fullPath;
+        // do something interesting with the file
+		return ruta;
+}, captureError, {limit:1});
 
 
 
-return captureSuccess;
+//return ruta;
 }
